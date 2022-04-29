31be426191aab0ff22be6dfd272f131f10a9e4d0
jeidiiy <wldyd2356@gmail.com>
2022-04-29 23:51:59 +0900

    feat: .env 설정 및 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
+import { MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev = process.env.MODE === 'dev' ? true : false;
+
+  configure(consumer: MiddlewareConsumer) {
+    mongoose.set('debug', this.isDev);
+  }
+}
